a5f6bfe63dc42bf5a858daefcd3d2a561d7f136d
akzize <akzakariae@gmail.com>
2024-08-18 00:13:03 +0100

    chore: Update installer script for Fedora and Ubuntu

--- installer.sh
+++ installer.sh
@@ -1,6 +1,9 @@
-sudo apt update
+# fedora
+# Update package manager
+sudo dnf update -y
 
-sudo apt install git curl wget gpg
+# Install necessary packages
+sudo dnf install git curl wget gnupg2 -y
 
 # git
 git config --global user.email "akziz2zakaria@gmail.com"
@@ -8,73 +11,47 @@ git config --global user.name "akzize"
 
 # github cli
 echo "install github cli: "
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+sudo dnf install 'dnf-command(config-manager)'
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli
 
 # chrome
 echo "install chrome: "
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-sudo dpkg --install google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf install ./google-chrome-stable_current_x86_64.rpm -y
 
 # vscode
 echo "install vscode: "
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+sudo dnf install code
 
 # telegram
 echo "install telegram-desktop: "
+sudo dnf install snapd -y
+sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install telegram-desktop
 
-
-
 # zsh
-
 echo "install zsh and ohmyzsh: "
-
-sudo apt install zsh
-
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
+sudo dnf install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
 
 # php
-#
+# echo "install php: "
+# sudo dnf install dnf-plugins-core -y
+# sudo dnf config-manager --set-enabled remi
+# sudo dnf install php php-cli php-pdo php-mysqlnd php-zip php-gd php-mbstring php-curl php-xml php-bcmath php-intl -y
 
-echo "install php: "
-sudo apt install curl gpg gnupg2 software-properties-common ca-certificates apt-transport-https lsb-release -y
+# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-sudo add-apt-repository ppa:ondrej/php\n
-
-sudo apt update -uy
-
-sudo apt -y install php8.3
-
-sudo apt install php8.3-{cli,pdo,mysql,zip,gd,mbstring,curl,xml,bcmath,common,intl}
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-
-sudo apt update
-
-sudo apt install apache2
-
-sudo apt install mysql-server-8.0
+# sudo dnf install httpd -y
+# sudo dnf install mysql-server -y
 
 
 
