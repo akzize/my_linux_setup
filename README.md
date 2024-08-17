befa2f0b7e9303aebc7cabb6dc89ad98fdfc7075
akzize <akzakariae@gmail.com>
2024-08-17 23:13:03 +0100

    chore: Add installation instructions for Chrome, VSCode, Telegram, Zsh, and PHP

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Linux Setup
+
+This repository contains the configuration files and scripts for my personal Linux setup. It includes various dotfiles, custom scripts, and installation instructions to help me quickly set up my preferred development environment on any Linux machine.
+
+Feel free to explore the different files and folders to see how I've customized my Linux setup. If you find anything useful, feel free to use it in your own setup as well.
+
+Happy coding!
--- installer.sh
+++ installer.sh
@@ -6,12 +6,24 @@ sudo apt install git curl wget gpg
 git config --global user.email "akziz2zakaria@gmail.com"
 git config --global user.name "akzize"
 
+# github cli
+echo "install github cli: "
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 # chrome
+echo "install chrome: "
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo apt install google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
 
 # vscode
+echo "install vscode: "
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
@@ -21,8 +33,17 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
 
+# telegram
+echo "install telegram-desktop: "
+sudo snap install telegram-desktop
+
+
+
 # zsh
 
+echo "install zsh and ohmyzsh: "
+
+sudo apt install zsh
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -30,10 +51,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugi
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 # php
 #
+
+echo "install php: "
 sudo apt install curl gpg gnupg2 software-properties-common ca-certificates apt-transport-https lsb-release -y
 
 sudo add-apt-repository ppa:ondrej/php\n
@@ -48,6 +71,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 sudo apt update
+
 sudo apt install apache2
 
 sudo apt install mysql-server-8.0
